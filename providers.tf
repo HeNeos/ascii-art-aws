c82cc009bf4df23537723563ea15bdfd5c483f27
HeNeos <heneos3.14@gmail.com>
2024-10-05 22:40:26 +0200

    Fix terraform states

--- providers.tf
+++ providers.tf
@@ -1,3 +1,7 @@
+data "aws_caller_identity" "current" {}
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket = "terraform-state-bucket-${data.aws_caller_identity.current.account_id}"
+    key    = "${var.stage}/infra.tfstate"
+  }
 }
