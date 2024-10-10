011ae7ed278763d6092531e8a44e278b3fffd034
HeNeos <heneos3.14@gmail.com>
2024-10-11 00:04:38 +0200

    Reduce image size and batch

--- lambdas/utils.py
+++ lambdas/utils.py
@@ -8,7 +8,7 @@ from lambdas.custom_types import ImageExtension
 
 
 def calculate_scale(image_height: int) -> int:
-    max_height = 90
+    max_height = 75
     new_scale: int = (image_height + max_height - 1) // max_height
     return new_scale
 
