e6a4cd5ab94445632be6b0281661ade2b016cff3
HeNeos <heneos3.14@gmail.com>
2024-10-12 15:16:57 +0200

    Add ecr lifecycle policy

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -4,22 +4,70 @@ terraform {
   backend "s3" {}
 }
 
+variable "ecr_lifecycle_policy" {
+  type    = string
+  default = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 3 images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["v"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 3
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
 resource "aws_ecr_repository" "downsize_media" {
   name = var.lambda_function_name_downsize_media
+}
+
+resource "aws_ecr_lifecycle_policy" "downsize_media" {
+  repository = aws_ecr_repository.downsize_media
+  policy     = var.ecr_lifecycle_policy
 }
 
 resource "aws_ecr_repository" "downsize_video" {
   name = var.lambda_function_name_downsize_video
 }
 
+resource "aws_ecr_lifecycle_policy" "downsize_media" {
+  repository = aws_ecr_repository.downsize_video
+  policy     = var.ecr_lifecycle_policy
+}
+
 resource "aws_ecr_repository" "extract_audio" {
   name = var.lambda_function_name_extract_audio
+}
+
+resource "aws_ecr_lifecycle_policy" "downsize_media" {
+  repository = aws_ecr_repository.extract_audio
+  policy     = var.ecr_lifecycle_policy
 }
 
 resource "aws_ecr_repository" "merge_frames" {
   name = var.lambda_function_name_merge_frames
 }
 
+resource "aws_ecr_lifecycle_policy" "downsize_media" {
+  repository = aws_ecr_repository.merge_frames
+  policy     = var.ecr_lifecycle_policy
+}
+
 resource "aws_ecr_repository" "process_frames" {
   name = var.lambda_function_name_process_frames
+}
+
+resource "aws_ecr_lifecycle_policy" "downsize_media" {
+  repository = aws_ecr_repository.process_frames
+  policy     = var.ecr_lifecycle_policy
 }
