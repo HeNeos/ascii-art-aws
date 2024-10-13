14777396e69f70e215f3324462012a09e0087014
HeNeos <heneos3.14@gmail.com>
2024-10-13 23:36:34 +0200

    Increase bitrate

--- lambdas/process_frames/lambda_function.py
+++ lambdas/process_frames/lambda_function.py
@@ -121,7 +121,7 @@ def lambda_handler(event, _) -> dict:
             "/tmp/temp-video.mp4",
             temp_audiofile="/tmp/null-audio.mp3",
             codec="libx264",
-            ffmpeg_params=["-g", "128", "-crf", "23", "-preset", "slow"],
+            ffmpeg_params=["-g", "128", "-crf", "23", "-preset", "slower"],
         )
         logger.info("Finish save local video")
         key = save_video(
