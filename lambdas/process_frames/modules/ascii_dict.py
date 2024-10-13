4b54ed43fe5f7c09348e14b519aa9ffeb279caf9
HeNeos <heneos3.14@gmail.com>
2024-10-13 19:50:07 +0200

    Test dict

--- lambdas/process_frames/modules/ascii_dict.py
+++ lambdas/process_frames/modules/ascii_dict.py
@@ -4,4 +4,5 @@ from enum import Enum
 class AsciiDict(Enum):
     BigDict = " `.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@"
     HighAsciiDict = " `.-,:!*|lowO#0@"
-    LowAsciiDict = " `.,:!|lw#@"
+    # LowAsciiDict = " `.,:!|lw#@"
+    LowAsciiDict = "X"
