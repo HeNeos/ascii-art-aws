8d629bce1b7435c47d2b53a7915500b1789e45c8
HeNeos <heneos3.14@gmail.com>
2024-10-13 13:24:10 +0200

    Reduce preset

--- lambdas/process_frames/lambda_function.py
+++ lambdas/process_frames/lambda_function.py
@@ -121,7 +121,7 @@ def lambda_handler(event, _) -> dict:
             "/tmp/temp-video.mp4",
             temp_audiofile="/tmp/null-audio.mp3",
             codec="libx264",
-            ffmpeg_params=["-g", "128", "-crf", "23", "-preset", "veryfast"],
+            ffmpeg_params=["-g", "128", "-crf", "23", "-preset", "faster"],
         )
         logger.info("Finish save local video")
         key = save_video(
