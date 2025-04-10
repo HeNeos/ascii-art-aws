1650c38e21c8a275382b99a3a7ca19f3f17d09fe
HeNeos <heneos3.14@gmail.com>
2025-04-11 01:13:11 +0200

    Secure bucket

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -133,3 +133,47 @@ resource "aws_s3_object" "r2_secrets" {
   server_side_encryption = "AES256"
   etag                   = filemd5("r2_secrets.json")
 }
+
+resource "aws_s3_bucket_public_access_block" "r2_secrets" {
+  bucket = aws_s3_bucket.r2_secrets.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+data "aws_iam_policy_document" "r2_secrets" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.r2_secrets.arn}/r2_secrets.json"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "aws:PrincipalArn"
+      values   = ["arn:aws:iam::${var.account_id}:role/*"]
+    }
+  }
+  statement {
+    effect    = "Deny"
+    actions   = ["s3:*"]
+    resources = [aws_s3_bucket.r2_secrets.arn, "${aws_s3_bucket.r2_secrets.arn}/*"]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "r2_secrets" {
+  bucket = aws_s3_bucket.r2_secrets.id
+  policy = data.aws_iam_policy_document.r2_secrets.json
+}
